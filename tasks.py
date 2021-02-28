f4a77a7acaf7465738e8b7e6751b0574014d5402
Matt Deitke <mdeitke23@gmail.com>
2021-02-27 16:37:31 -0800

    add set-version task

--- tasks.py
+++ tasks.py
@@ -2,5 +2,11 @@ from invoke import task
 
 
 @task
-def set_version(c):
-    print("hello, world!")
+def set_version(c, version):
+    """Writes the version upon a release."""
+    for filename in ["setup.py", "ai2thor_colab/__init__.py"]:
+        with open(filename, "r") as f:
+            file = f.read()
+        file = file.replace("<REPLACE_WITH_VERSION>", version)
+        with open(filename, "w") as f:
+            f.write(file)
